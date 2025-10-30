64bfe1deec96b9e33bdffd3acaa79824e2fa4c05
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2025-10-30 13:55:04 +0530

    recursion

---
+++ C/16pro.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int sum (int arr[], int n){
+    if(n==0){
+        return 0;
+    }
+    return arr[n-1]+sum(arr,n-1);
+ }
+    int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5;
+    int data= sum(arr,n);
+    printf("%d",data);
+}         
