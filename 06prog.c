5f81be98b7e1adce3add336d2a654b290276dc5a
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2025-12-13 19:06:40 +0530

    first commit

---
+++ 06prog.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int sum(int a, int b){
+printf("%d",a-b);
+
+ } 
+ int main(){
+   // int a ;
+   // scanf("%d",&a);
+sum(12,20);
+return 0;
+printf("yasu");
+ }
---
+++ 07prog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int sum(){
+    int a=12;
+    int b=13;
+    int c=a+b;
+    return c;
+}
+int main(){
+  int data = sum(); 
+  printf("%d",data);
+}
