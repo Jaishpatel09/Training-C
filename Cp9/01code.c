a7a1501cef14b43ba8f5ae57c46e8802197d173f
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2025-12-19 00:31:34 +0530

    Structure

---
+++ Cp9/01code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+struct employee
+{
+    int code;
+    float salary;
+    char name[30];
+};
+int main()
+{
+    struct employee e1, e2;
+    e1.code = 799;
+    e1.salary = 55.21;
+    strcpy(e1.name, "Jaish");
+
+    printf("%d %f %s", e1.code, e1.salary, e1.name);
+    return 0;
+}
